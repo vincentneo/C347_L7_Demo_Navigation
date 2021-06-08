82c9660e48756e197a9b318eff5afc88f75d600f
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-06-08 11:55:57 +0800

    up to section C

---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demonavigation/NewActivity.java
@@ -0,0 +1,20 @@
+package sg.edu.rp.c347.id19007966.demonavigation;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
